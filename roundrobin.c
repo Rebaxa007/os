cd23adf47adbc49efcbe85883f751dcbd6670f07
Rebaxa007 <SWARNABHA0000@protonmail.com>
2024-11-21 10:39:01 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -1,67 +1,89 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-struct Process {
-    int pid;            // Process ID
-    int arrivalTime;    // Arrival Time
-    int burstTime;      // Burst Time
-    int remainingTime;  // Remaining Burst Time
-    int completionTime; // Completion Time
-    int turnAroundTime; // Turnaround Time
-    int waitingTime;    // Waiting Time
-};
+void calculateRoundRobin(int n, int at[], int bt[], int tq) {
+    int wt[n], tat[n], ct[n], remaining_bt[n];
+    int time = 0, completed = 0;
+    bool isProcessInQueue = true;
+    
+    // Copy burst times to remaining burst time array
+    for (int i = 0; i < n; i++) {
+        remaining_bt[i] = bt[i];
+        wt[i] = 0;
+        ct[i] = 0;
+    }
 
-void roundRobin(struct Process p[], int n, int timeQuantum) {
-    int currentTime = 0, completed = 0;
+    printf("\nGantt Chart:\n");
     while (completed < n) {
+        isProcessInQueue = false;
+
         for (int i = 0; i < n; i++) {
-            if (p[i].remainingTime > 0 && p[i].arrivalTime <= currentTime) {
-                if (p[i].remainingTime > timeQuantum) {
-                    currentTime += timeQuantum;
-                    p[i].remainingTime -= timeQuantum;
+            if (at[i] <= time && remaining_bt[i] > 0) {
+                isProcessInQueue = true;
+
+                printf("P%d | ", i + 1);
+
+                if (remaining_bt[i] > tq) {
+                    time += tq;
+                    remaining_bt[i] -= tq;
                 } else {
-                    currentTime += p[i].remainingTime;
-                    p[i].remainingTime = 0;
-                    p[i].completionTime = currentTime;
-                    p[i].turnAroundTime = p[i].completionTime - p[i].arrivalTime;
-                    p[i].waitingTime = p[i].turnAroundTime - p[i].burstTime;
+                    time += remaining_bt[i];
+                    remaining_bt[i] = 0;
+                    ct[i] = time;
                     completed++;
                 }
             }
         }
+        if (!isProcessInQueue) time++;
     }
-}
+    printf("\n");
 
-void display(struct Process p[], int n) {
-    printf("\nPID\tArrival\tBurst\tCompletion\tTurnAround\tWaiting\n");
+    // Calculate TAT and WT
+    float avg_tat = 0, avg_wt = 0;
     for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t\t%d\t\t%d\n",
-               p[i].pid, p[i].arrivalTime, p[i].burstTime,
-               p[i].completionTime, p[i].turnAroundTime, p[i].waitingTime);
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        avg_tat += tat[i];
+        avg_wt += wt[i];
     }
+
+    avg_tat /= n;
+    avg_wt /= n;
+
+    // Print Results
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+
+    printf("\nAverage Turnaround Time: %.2f\n", avg_tat);
+    printf("Average Waiting Time: %.2f\n", avg_wt);
 }
 
 int main() {
-    int n, timeQuantum;
+    int n, tq;
 
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    struct Process p[n];
+    int at[n], bt[n];
 
+    printf("Enter the arrival times of the processes:\n");
     for (int i = 0; i < n; i++) {
-        p[i].pid = i + 1; // Process ID starts from 1
-        printf("Enter Arrival Time for Process %d: ", p[i].pid);
-        scanf("%d", &p[i].arrivalTime);
-        printf("Enter Burst Time for Process %d: ", p[i].pid);
-        scanf("%d", &p[i].burstTime);
-        p[i].remainingTime = p[i].burstTime; // Initialize remaining time
+        printf("P%d: ", i + 1);
+        scanf("%d", &at[i]);
     }
 
-    printf("Enter Time Quantum: ");
-    scanf("%d", &timeQuantum);
+    printf("Enter the burst times of the processes:\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
 
-    roundRobin(p, n, timeQuantum);
-    display(p, n);
+    printf("Enter the time quantum: ");
+    scanf("%d", &tq);
+
+    calculateRoundRobin(n, at, bt, tq);
 
     return 0;
 }
